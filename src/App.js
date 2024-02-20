1ea530eb5ce0f161c41357a5264cb9c531e35191
Mikhail4ch <mihamakarevi426@gmail.com>
2024-02-20 19:09:52 +0300

    refreshing of access token [no success so far]

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import RequireAuth from "./components/requireAuth";
-import { Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Main from './components/mainPage/mainPage';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
 import WeatherApp from "./weatherApp";
-import { useEffect} from "react";
 import useAuth from "./components/hooks/useAuth";
 import connection from "./components/connection/connection";
 import Layout from "./components/Layout";
 import Missing from "./components/Missing";
-import { useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import Access from "./components/mainPage/access";
+import PersistLogin from "./components/persistLogin";
 
 
 
@@ -18,11 +18,11 @@ function App() {
 
   const { setAuth } = useAuth();
   const navigate = useNavigate();
- 
+
   const createUser = async (itemEmail, itemPassword) => {
     try {
       const response = await connection.post('/user/register', { email: itemEmail, password: itemPassword })
-      console.log(response.data);
+      console.log(response.data)
       if (response.data) {
         alert("Account was successfuly created !")
       }
@@ -31,9 +31,7 @@ function App() {
     }
   }
 
-  useEffect(() => {
-    createUser()
-  }, [])
+
 
   const validateUser = async (itemEmail, itemPassword) => {
     try {
@@ -44,13 +42,13 @@ function App() {
         });
       console.log(JSON.stringify(response?.data));
       const accessToken = response?.data?.accessToken;
-      setAuth({accessToken})
+      setAuth({ accessToken })
 
       if (response.status) {
         alert("Enjoy the app!")
       }
       navigate('/App');
-     
+
     } catch (error) {
       console.log(error.message)
     }
@@ -68,8 +66,10 @@ function App() {
         <Route path="/SignIn" element={<Login handleLogin={validateUser} />} />
         <Route path="/SignUp" element={<Register handleRegister={createUser} />} />
         {/* protected route */}
-        <Route element={<RequireAuth />}>
-          <Route path="/App" element={<WeatherApp />} />
+        <Route element={<PersistLogin />}>
+          <Route element={<RequireAuth />}>
+            <Route path="/App" element={<WeatherApp />} />
+          </Route>
         </Route>
         {/* catch all */}
         <Route path="*" element={<Missing />} />
