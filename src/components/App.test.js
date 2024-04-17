139a2883824a99deb54606dde0973a96cd1fa27d
Mikhail4ch <mihamakarevi426@gmail.com>
2024-04-17 15:23:33 +0300

    Testing component

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,3 +1,15 @@
+import CurrentWeather from "./current-weather/current-weather";
+import { render, screen } from '@testing-library/react'
+
+test ('Testing spans in current-weather component', () => {
+  render(<CurrentWeather/>);
+
+  const element = screen.getByText(/details/i);
+
+  expect(element).toBeInTheDocument();
+})
+
+
 test('renders learn react link', () => {
     expect(true).toBe(true);
   });
