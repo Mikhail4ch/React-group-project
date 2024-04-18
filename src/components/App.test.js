6215f87941a067089cc6c336504f5f6fd8dd0f68
Mikhail4ch <mihamakarevi426@gmail.com>
2024-04-18 10:55:10 +0300

    import '@testing-library/jest-dom'

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,5 +1,6 @@
 import CurrentWeather from "./current-weather/current-weather";
-import { render, screen } from '@testing-library/react'
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom'
 
 const mockData = {
   "city": "Oslo",
