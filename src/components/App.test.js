505809e100467584b94d22c06f769e4c0f50e76e
Mikhail4ch <mihamakarevi426@gmail.com>
2024-04-18 15:58:12 +0300

    Testing multiple spans

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -6,33 +6,28 @@ import '@testing-library/jest-dom';
 const mockData = {
   "city": "Oslo",
   "weather": [
-  {
-    "description": "overcast clouds",
-    "icon": "grey cloud"
-  }
+    {
+      "description": "overcast clouds",
+      "icon": "grey cloud"
+    }
   ],
   "main": {
-  "temp": 15,
-  "feels_like": 9,
-  "humidity": 20,
-  "pressure": 1140
+    "temp": 15,
+    "feels_like": 9,
+    "humidity": 20,
+    "pressure": 1140
   },
   "wind": {
-  "speed": 1
+    "speed": 1
   }
-  };
+};
 
-test ('Testing spans in current-weather component', () => {
-  render(<CurrentWeather data = {mockData}/>)
-  expect(screen.getByText(/Details/)).toBeInTheDocument();
-  
-const spans = ['Details', 'Feels like', '%', 'hPa'];
-
-spans.forEach(span => {
-
-expect(screen.getByText(new RegExp(span, 'i'))).toBeInTheDocument();
-
-});
+test('Testing spans in current-weather component', () => {
+  render(<CurrentWeather data={mockData} />)
+  const spans = ['Details', 'Feels like', '%', 'hPa'];
+  spans.forEach(span => {
+    expect(screen.getByText(new RegExp(span, 'i'))).toBeInTheDocument();
+  });
   expect(screen.getAllByText(/°C/)).toBeTruthy();
 })
 
@@ -40,5 +35,5 @@ expect(screen.getByText(new RegExp(span, 'i'))).toBeInTheDocument();
 
 
 test('renders learn react link', () => {
-    expect(true).toBe(true);
-  });
+  expect(true).toBe(true);
+});
