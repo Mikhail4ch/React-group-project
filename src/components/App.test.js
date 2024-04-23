4c963a588a5d39c7e5803a250fa5e12de52f3db3
Mikhail4ch <mihamakarevi426@gmail.com>
2024-04-23 21:59:38 +0300

    fixed imports

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,11 +1,12 @@
 import CurrentWeather from "./current-weather/current-weather";
-import { render, screen, fireEvent, userEvent, waitFor } from '@testing-library/react';
+import { render, screen} from '@testing-library/react';
+/*import { render, screen, fireEvent, userEvent, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Forecast from "./forecast/forecast";
 import Main from "./mainPage/mainPage";
 import Register from "./Register/Register";
 import { describe, expect, it } from "vitest";
-import * as services from '../weatherApp'
+import * as services from '../weatherApp'*/
 
 
 
