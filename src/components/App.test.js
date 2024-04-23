b7de750f48c77c6f5cfb1fb1b12b881b4f16eaab
Mikhail4ch <mihamakarevi426@gmail.com>
2024-04-23 21:46:23 +0300

    added API tests and commented out errors

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,9 +1,12 @@
 import CurrentWeather from "./current-weather/current-weather";
-import { render, screen, fireEvent, userEvent } from '@testing-library/react';
+import { render, screen, fireEvent, userEvent, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Forecast from "./forecast/forecast";
 import Main from "./mainPage/mainPage";
 import Register from "./Register/Register";
+import { describe, expect, it } from "vitest";
+import * as services from '../weatherApp'
+
 
 
 // Test for the current-weather component using mockData 
@@ -76,17 +79,17 @@ test('Testing labels in forecast component', async () => {
     fireEvent.click(button);
   }
 
-const labels = ['Daily', '°C'];
+  const labels = ['Daily', '°C'];
 
-for (const label of labels) {
-  expect(await screen.findByText(new RegExp(label, 'i'))).toBeInTheDocument();
+  for (const label of labels) {
+    expect(await screen.findByText(new RegExp(label, 'i'))).toBeInTheDocument();
   }
 });
 
 // Testing content presence on mainPage
 
-test ("There h3 on the mainPage", () => {
-  render(<Main/>)
+test("There h3 on the mainPage", () => {
+  render(<Main />)
   const h3 = ["Homepage", "Weather app"];
   h3.forEach(h => {
     expect(screen.getByText(new RegExp(h, 'i'))).toBeInTheDocument();
@@ -95,7 +98,7 @@ test ("There h3 on the mainPage", () => {
 
 // Testing interaction on mainPage
 
-test ("testing a click on a link", async () => {
+test("testing a click on a link", async () => {
   render(<Main />)
   await userEvent.click(screen.getByText(/Weather app/));
   expect(userEvent).toHaveBeenCalled();
@@ -103,25 +106,112 @@ test ("testing a click on a link", async () => {
 
 // Testing Register component 
 
-test ("testing input fields of Register component", async () => {
-  render(<Register/>)
+test("testing input fields of Register component", async () => {
+  render(<Register />)
   const inputValues = ["Enter your email", "Enter your password"];
- for (const input of inputValues) {
-  expect(screen.getByDisplayValue(new RegExp(input, 'i'))).toBeInTheDocument();
- }
+  for (const input of inputValues) {
+    expect(screen.getByDisplayValue(new RegExp(input, 'i'))).toBeInTheDocument();
+  }
 })
 
-test ("testing button of Register component", async () => {
-  render(<Register/>) 
-  const createButton= screen.getByRole('button', { name: /Create/});
+test("testing button of Register component", async () => {
+  render(<Register />)
+  const createButton = screen.getByRole('button', { name: /Create/ });
   expect(createButton).toBeInTheDocument();
 })
 
-test ("testing a click on a Create button", async () => {
-  render(<Register/>)
+test("testing a click on a Create/ buttons", async () => {
+  render(<Register />)
   await userEvent.click(screen.getByText(/Create/));
   expect(userEvent).toHaveBeenCalled();
 })
 
-// Test state updates 
+describe("testing state updates of input fields", () => {
+  it("work", () => {
+    let isCallbackCalled = false;
 
+    const setInputValue = () => {
+      isCallbackCalled = true;
+    };
+
+    render(
+      <Register onChange={setInputValue} />
+    );
+    const inputs = ["Email", "Password"]
+    for (const input of inputs) {
+      fireEvent.change(screen.getByLabelText(new RegExp(input, 'i')), { target: { value: "1" } })
+    };
+    expect(isCallbackCalled).toBe(true);
+  })
+})
+
+// Testing API call in current-weather component 
+
+const mockFetchData = jest.spyOn(services, 'FetchData')
+  .mockImplementation(async () => {
+    return [{
+      "city": "Oslo",
+      "weather": [
+        {
+          "description": "overcast clouds",
+          "icon": "grey cloud"
+        }
+      ],
+      "main": {
+        "temp": 15,
+        "feels_like": 9,
+        "humidity": 20,
+        "pressure": 1140
+      },
+      "wind": {
+        "speed": 1
+      }
+    }];
+  })
+
+render(<CurrentWeather/>)
+expect(mockFetchData).toHaveBeenCalled(); 
+const data = ["grey cloud", 1140, "Oslo", "1"]
+await waitFor(() => { for (const dat of data) {
+  expect(screen.getByText(new RegExp(dat, 'i'))).toBeInTheDocument();
+}}) 
+
+// Testing API call in forecast component 
+
+const mockFetchData2 = jest.spyOn(services, 'FetchData')
+  .mockImplementation(async () => {
+    return [{
+      "item": [
+        {
+          "weather": [
+            {
+              "description": "broken clouds",
+              "icon": "bright cloud"
+            }
+          ],
+          "main": [
+            {
+              "temp_min": 5
+            }
+          ]
+        }
+      ],
+      "index": 2,
+      "list": [
+        {
+          "splice": [
+            {
+              "map": "whatever"
+            }
+          ]
+        }
+      ]
+    }];
+  })
+
+render(<Forecast/>)
+expect(mockFetchData2).toHaveBeenCalled(); 
+const data2 = ["whatever", 2, "bright cloud", 5]
+await waitFor(() => { for (const dat of data2) {
+  expect(screen.getByText(new RegExp(dat, 'i'))).toBeInTheDocument();
+}}) 
