e8c57de01c8d1065099db32326b2c0deb8617770
Mikhail4ch <mihamakarevi426@gmail.com>
2024-04-24 12:05:32 +0300

    Trying to spy on Promise

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -8,7 +8,7 @@ import Register from "./Register/Register";
 import { describe, expect, it } from "vitest";*/
 import Forecast from "./forecast/forecast";
 import * as services from  '../weatherApp'
-import WeatherApp from "../weatherApp";
+
 
 
 
@@ -152,7 +152,7 @@ describe("testing state updates of input fields", () => {
 
 // Testing API call in current-weather component 
 
-const mockFetchData = jest.spyOn(services, WeatherApp)
+const mockFetchData = jest.spyOn(services, 'Promise')
   .mockImplementation(async () => {
     return [{
       "city": "Oslo",
@@ -183,7 +183,7 @@ await waitFor(() => { for (const dat of data) {
 
 // Testing API call in forecast component 
 
-const mockFetchData2 = jest.spyOn(services, WeatherApp)
+const mockFetchData2 = jest.spyOn(services, 'Promise')
   .mockImplementation(async () => {
     return [{
       "item": [
